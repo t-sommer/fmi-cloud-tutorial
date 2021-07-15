138de504aa0b2287ee818312dbaaea538e48c531
Torsten Sommer <torsten.sommer@3ds.com>
2021-07-15 17:40:36 +0200

    Add heater.py

---
+++ heater.py
@@ -0,0 +1,11 @@
+""" Example of a library to be used in the Jupyter Notebook """
+
+from fmpy import *
+
+
+def simulate_heater(TAmb=293.15, stop_time=100):
+    """ Helper function with a fixed set of parameters """
+
+    result = simulate_fmu('Heater.fmu', stop_time=stop_time)
+
+    plot_result(result)
