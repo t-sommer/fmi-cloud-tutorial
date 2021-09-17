6a47d3a45be3449dc95f29bc8a114dc4c290c7f0
Torsten Sommer <torsten.sommer@3ds.com>
2021-09-17 13:41:25 +0200

    Fix heater.py

--- heater.py
+++ heater.py
@@ -6,6 +6,6 @@ from fmpy import *
 def simulate_heater(TAmb=293.15, stop_time=100):
     """ Helper function with a fixed set of parameters """
 
-    result = simulate_fmu('Heater.fmu', stop_time=stop_time)
+    result = simulate_fmu('Heater.fmu', stop_time=stop_time, start_values={'TAmb': TAmb})
 
     plot_result(result)
