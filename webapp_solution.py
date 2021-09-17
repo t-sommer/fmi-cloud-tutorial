f0725f20e4a141081b18ba67818c83411a89e48f
Torsten Sommer <torsten.sommer@3ds.com>
2021-09-17 11:33:08 +0200

    Add webapp

---
+++ webapp_solution.py
@@ -0,0 +1,115 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+
+from fmpy import simulate_fmu, read_model_description
+from fmpy.util import create_plotly_figure
+
+import numpy as np
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+model_description = read_model_description('Heater.fmu')
+
+
+app.layout = dbc.Container([
+    dbc.Container(
+        [
+            html.Img(src=app.get_asset_url('Heater.png'), style={'max-width': '80%', 'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'})
+        ],
+        className='my-5'
+    ),
+    dbc.Container(
+        [
+            dbc.Alert(model_description.description, color='dark')
+        ],
+        className='my-5'
+    ),
+    dbc.Container(
+        [
+            dbc.Form(
+                [
+                    dbc.Button('Simulate', id='simulate-button', color='primary', className='mr-4'),
+                    dbc.InputGroup(
+                        [
+                            dbc.Input(id='stop-time', value='50', style={'text-align': 'right', 'width': '5rem'}),
+                            dbc.InputGroupAddon('s', addon_type="append", style={'width': '2rem'})
+                        ], className='mr-4'
+                    )
+                ], inline=True
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            dbc.FormGroup(
+                                [
+                                    dbc.Label('TAmb', html_for='TAmb', width=6),
+                                    dbc.Col(
+                                        dbc.InputGroup(
+                                            [
+                                                dbc.Input(id='TAmb', value='283.15', style={'text-align': 'right'}),
+                                                dbc.InputGroupAddon('K', addon_type='append'),
+                                            ], size='sm'
+                                        ),
+                                        width=6,
+                                    ),
+                                    html.Small("Ambient temperature", className='form-text text-muted ml-3')
+                                ],
+                                row=True, className='mb-2'
+                            ),
+                            dbc.FormGroup(
+                                [
+                                    dbc.Label('V', html_for='V', width=6),
+                                    dbc.Col(
+                                        dbc.InputGroup(
+                                            [
+                                                dbc.Input(id='V', value='10', style={'text-align': 'right'}),
+                                                dbc.InputGroupAddon('V', addon_type='append'),
+                                            ], size='sm'
+                                        ),
+                                        width=6,
+                                    ),
+                                    html.Small("Voltage", className='form-text text-muted ml-3')
+                                ],
+                                row=True, className='mb-2'
+                            ),
+                        ],
+                        width=12, lg=4, style={'margin-top': '2rem'}
+                    ),
+                    dbc.Col(id='result-col', width=12, lg=8),
+                ], className='mt-4'
+            ),
+        ],
+        id='simulation-container'
+    ),
+
+])
+
+
+@app.callback(
+    Output('result-col', 'children'),
+    [Input('simulate-button', 'n_clicks')],
+    [State('stop-time', 'value'), State('TAmb', 'value'), State('V', 'value')]
+)
+def update_output_div(n_clicks, stop_time, TAmb, V):
+
+    try:
+        input = np.array([(0, False), (10, True), (20, False)], dtype=[('time', np.float64), ('u', np.bool_)])
+        result = simulate_fmu(
+            filename='Heater.fmu',
+            stop_time=stop_time,
+            start_values={'TAmb': TAmb, 'constantVoltage.V': V},
+            output=['T', 'u'],
+            input=input
+        )
+        fig = create_plotly_figure(result=result)
+        return dcc.Graph(figure=fig)
+    except Exception as e:
+        return dbc.Alert("Simulation failed. %s" % e, color='danger'),
+
+
+if __name__ == "__main__":
+    app.run_server()
